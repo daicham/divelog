22469c72ccc08d930075d4b1c515bfc99b624678
daicham <daicham@gmail.com>
2015-12-30 23:42:36 +0900

    Add message for registry, update, delete

--- src/main/java/com/daicham/divelog/web/LogController.java
+++ src/main/java/com/daicham/divelog/web/LogController.java
@@ -6,9 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.validation.Valid;
 
@@ -28,17 +30,17 @@ public class LogController {
     }
 
     @RequestMapping(path = "new", method = RequestMethod.GET)
-    public String _new(Model model) {
-        model.addAttribute("log", new Log());
+    public String _new(@ModelAttribute Log log) {
         return "logs/new";
     }
 
     @RequestMapping(path = "new", method = RequestMethod.POST)
-    public String register(@Valid Log log, BindingResult result) {
+    public String register(@Valid Log log, BindingResult result, RedirectAttributes attributes) {
         if (result.hasErrors()) {
             return "logs/new";
         }
         service.register(log);
+        attributes.addFlashAttribute("message", "messages.success.registry");
         return "redirect:/logs";
     }
 
@@ -55,17 +57,19 @@ public class LogController {
     }
 
     @RequestMapping(path = "{id}", method = RequestMethod.POST)
-    public String update(@Valid Log log, BindingResult result) {
+    public String update(@Valid Log log, BindingResult result, RedirectAttributes attributes) {
         if (result.hasErrors()) {
             return "logs/edit";
         }
         service.update(log);
-        return "redirect:/logs";
+        attributes.addFlashAttribute("message", "messages.success.update");
+        return "redirect:/logs/" + log.getId();
     }
 
     @RequestMapping(path = "{id}/delete", method = RequestMethod.POST)
-    public String delete(@PathVariable Long id) {
+    public String delete(@PathVariable Long id, RedirectAttributes attributes) {
         service.deleteById(id);
+        attributes.addFlashAttribute("message", "messages.success.delete");
         return "redirect:/logs";
     }
 }
